3c83e9924bdb8bd82dda3a298dfd9cf4ebe8889a
Nate Cadicamo <natecadicamo@gmail.com>
2025-11-09 13:41:47 -0800

    fix: get apps route

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 
 from .chat_routes import router as chat_router
 from .health import router as health_router
+from .application_routes import router as application_router
 
 router = APIRouter()
 router.include_router(health_router, prefix="/api", tags=["health"])
 router.include_router(chat_router, prefix="/api", tags=["chat"])
+router.include_router(application_router, prefix="/api", tags=["applications"])
 
 __all__ = ["router"]
